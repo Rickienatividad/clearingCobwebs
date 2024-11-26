106da8ffb0a939c1858e72dfe54b21ca2d44d2d0
Rickie Natividad <rickienatividad85@gmail.com>
2024-11-26 09:27:00 -0500

    initial commit

---
+++ react-springboot-stack-backend/src/test/java/com/react_springboot_stack/react_springboot_stack_backend/ReactSpringbootStackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react_springboot_stack.react_springboot_stack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootStackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
