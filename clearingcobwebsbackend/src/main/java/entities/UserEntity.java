e1bf0fc13c7f5943baf08e493fb406a78136cfa3
Rickie Natividad <rickienatividad85@gmail.com>
2024-11-29 10:05:10 -0500

    adds directories for controllers, entities, models, repositories, and services

---
+++ clearingcobwebsbackend/src/main/java/entities/UserEntity.java
@@ -0,0 +1,33 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "users")
+public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String email;
+
+  private String password;
+
+}
