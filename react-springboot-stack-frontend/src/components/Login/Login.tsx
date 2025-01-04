7f0c10f4735fa2972b8f508758c4e9635cb58f4a
Rickie Natividad <rickienatividad85@gmail.com>
2025-01-04 14:05:00 -0600

    makes login failure message display appropriately on front end

--- react-springboot-stack-frontend/src/components/Login/Login.tsx
+++ react-springboot-stack-frontend/src/components/Login/Login.tsx
@@ -1,9 +1,12 @@
 import { NavLink } from 'react-router'
 import './Login.css'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import axios from 'axios';
 
 export function Login() {
+
+  const [isLoginValid, setIsLoginValid]= useState(true);
+
   const handleLogin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const params: FormData = new FormData(e.currentTarget);
@@ -19,7 +22,8 @@ export function Login() {
         console.log(response);
       })
       .catch((error) => {
-        console.log(error.message)
+        //console.log(error.status)
+        if(error.status == "403") {setIsLoginValid(false)}
       })
   }
   return(
@@ -29,15 +33,15 @@ export function Login() {
       <form onSubmit={handleLogin}>
         <input type="text" name="email" placeholder="email" className="form-control-lg"/>
         <input type="password" name="password" placeholder="password" className="form-control-lg"/>
+        <div hidden={isLoginValid} style={{marginTop: '-1.2rem'}}>
+          <h5 style={{color:'#a62a17'}}>Password was incorrect.</h5>
+        </div>
         <button type="submit">Enter</button>
       </form>
       <div style={{textAlign: 'right', marginTop: '1.3rem', marginRight: '-5.5rem'}}>
         <p style={{display:'inline', marginRight: '1rem'}}> Not a member?</p>
         <NavLink to="/">Sign Up!</NavLink>
       </div>
-    </div>
-    <div>
-      <h5 style={{color:'#a62a17'}}>Password was incorrect.</h5>
     </div>
     </>
   )
