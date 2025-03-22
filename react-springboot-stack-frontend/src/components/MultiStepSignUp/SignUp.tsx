c4592af521e9a82321144bac104f72160630f152
Rickie Natividad <rickienatividad85@gmail.com>
2025-03-22 17:11:36 -0500

    resizes step indicators

--- react-springboot-stack-frontend/src/components/MultiStepSignUp/SignUp.tsx
+++ react-springboot-stack-frontend/src/components/MultiStepSignUp/SignUp.tsx
@@ -41,21 +41,21 @@ export function SignUp() {
     <div className="container">
       <div id="steps-section">
         <div id="step-one-line" className={"step-line-active"}>
-          <span id="step-one-dot" className={"step-dot-active"}>Step One</span>
+          <span id="step-one-dot" className={"step-dot-active"}>Name/Email</span>
         </div>
         <div id="step-two-line" className={formPgRef.current >= 2 ? "step-line-active" : "step-line"}>
-          <span id="step-two-dot" className={formPgRef.current >= 2 ? "step-dot-active" : "step-dot"}>Step Two</span>
+          <span id="step-two-dot" className={formPgRef.current >= 2 ? "step-dot-active" : "step-dot"}>Password</span>
         </div>
         <div id="step-three-line" className={formPgRef.current == 3 ? "step-line-active" : "step-line"}>
-          <span id="step-three-dot" className={formPgRef.current == 3 ? "step-dot-active" : "step-dot"}>Step Three</span>
+          <span id="step-three-dot" className={formPgRef.current == 3 ? "step-dot-active" : "step-dot"}>Security Questions</span>
         </div>
       </div>
       <div style={{visibility: formPgRef.current == 1 ? "visible" : "hidden"}}><StepOne /></div>
       <div style={{visibility: formPgRef.current == 2 ? "visible" : "hidden"}}><StepTwo /></div>
       <div style={{visibility: formPgRef.current == 3 ? "visible" : "hidden"}}><StepThree secQuestions={securityQuestions}/></div>
       <div className="button-container">
-      <button className="form-btn" onClick={shiftFormBack}>Back</button>
-      <button className="form-btn" onClick={shiftFormUp}>Next</button>
+        <button className="form-btn" onClick={shiftFormBack}>Back</button>
+        <button className="form-btn" onClick={shiftFormUp}>Next</button>
     </div>
     </div>
   )
